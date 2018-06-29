a9400e48f4edc557173d85c024f2c38ac9874b53
tianxiazhizhong <tianxiazhizhong@gmail.com>
2018-06-29 11:34:24 +0800

    1.0.0

--- tpl/index.js
+++ tpl/index.js
@@ -1,5 +1,7 @@
-process.env.NODE_ENV = 'production';
-
+if(process.env.NODE_ENV !== 'production'){
+  process.env.NODE_ENV = 'production';
+};
+console.log('linux-remote start!');
 const server = require('linux-remote-server');
 const client = require('linux-remote-client');
 const config = require('./config');
