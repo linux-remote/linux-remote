47e68008fa50b3df69bfe61fbad79b229b0c204b
tianxiazhizhong <tianxiazhizhong@gmail.com>
2019-03-26 20:38:11 +0800

    dev

---
+++ dev.js
@@ -0,0 +1,10 @@
+if(process.env.NODE_ENV !== 'production'){
+  process.env.NODE_ENV = 'production';
+};
+console.log('linux-remote start!');
+const server = require('../linux-remote-server');
+const client = require('../linux-remote-client');
+const config = require('./config');
+
+config.client = client;
+server(config);
