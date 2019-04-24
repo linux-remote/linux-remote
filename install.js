1a013016eded5e9e8f8efcc90ac64d46bbe7fd2c
tianxiazhizhong <tianxiazhizhong@gmail.com>
2019-04-24 20:57:00 +0800

    i

---
+++ install.js
@@ -0,0 +1,17 @@
+console.log('install');
+// #!/usr/bin/env node
+// const os = require('os');
+// const { execSync } = require('child_process');
+// const fs = require('fs');
+// const path = require('path');
+// const crypto = require('crypto');
+
+// const userName = 'linux-remote';
+
+// let tmp_dir = `/dev/shm/${userName}`;
+
+// // user
+// execSync(`useradd --base-dir='/opt' ${userName}`);
+
+// // project dir
+// fs.mkdirSync(`/opt/${userName}`);
