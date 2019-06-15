ab23bec448f2a5e6428e894583db2d2d1bb7cbad
tianxiazhizhong <tianxiazhizhong@gmail.com>
2019-06-16 01:19:03 +0800

    more setting

--- init.js
+++ init.js
@@ -1,11 +1,10 @@
-var crypto = require('crypto');
 var path = require('path');
 var {execSync} = require('child_process');
 var fs = require('fs');
-
+const uuidv4 = require('uuid/v4');
 var tplStr = fs.readFileSync(path.join(__dirname, 'tpl.config.js'), 'utf-8');
 
-var secret = crypto.randomBytes(18).toString('base64') + Date.now();
+var secret = uuidv4();
 tplStr = tplStr.replace('{{sessionSecret}}', secret);
 
 var configPath = path.join(__dirname, 'config.js');
