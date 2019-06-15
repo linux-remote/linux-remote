8c3f508cf0a77558730f44a9c6801f76b058450e
tianxiazhizhong <tianxiazhizhong@gmail.com>
2019-06-15 14:44:36 +0800

    linux-remote-user-server

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ if(process.env.NODE_ENV !== 'production'){
 };
 console.log('linux-remote start!');
 const server = require('linux-remote-server');
-const userServer = require('@linux-remote/user-server');
+const userServer = require('linux-remote-user-server');
 const client = require('linux-remote-client');
 
 const config = require('./config');
