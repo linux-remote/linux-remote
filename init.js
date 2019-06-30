b07b8cdd5a23f906d31e062e4ed94e605e728792
tianxiazhizhong <tianxiazhizhong@gmail.com>
2019-06-30 21:39:12 +0800

    remove uuid

--- init.js
+++ init.js
@@ -1,10 +1,10 @@
 var path = require('path');
 var {execSync} = require('child_process');
 var fs = require('fs');
-const uuidv4 = require('uuid/v4');
+const crypto = require('crypto');
 var tplStr = fs.readFileSync(path.join(__dirname, 'tpl.config.js'), 'utf-8');
 
-var secret = uuidv4();
+var secret = crypto.randomBytes(26).toString('base64');
 tplStr = tplStr.replace('{{sessionSecret}}', secret);
 
 var configPath = path.join(__dirname, 'config.js');
