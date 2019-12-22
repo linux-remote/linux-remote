4192ce1a7f3fc61c9a59a484e89f1b2085d892a0
tianxiazhizhong <tianxiazhizhong@gmail.com>
2019-12-22 22:03:19 +0800

    init

---
+++ lib/cpp/lr-login.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+using namespace std;
+int main(int argc, char* argv[])
+{
+
+        int i;
+        string args = "";
+
+        for(i = 1; i < argc; i++){
+                args = args + " " + argv[i];
+        }
+        
+        args = "login" + args;
+        
+        system(args.c_str());
+        return 0;
+}
---
+++ tpl/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  port: 3001, // Website listen port. default: 3001
+
+  secure : false, // http model, Unsafe,  default: false.
+  
+  /*
+  // Or provide an Object to enter https model: 
+
+  secure: {
+    certPath: '/somedir/cert.pem',
+    keyPath: '/somedir/privkey.pem', 
+
+    //... Other options same as https://nodejs.org/api/tls.html#tls_tls_createsecurecontext_options
+  }
+  */
+};
