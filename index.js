d428a495fe9be1039b93edf1f5b457102f6cff26
tianxiazhizhong <tianxiazhizhong@gmail.com>
2019-12-31 23:40:32 +0800

    init uninit OK

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const init = require('./lib/init');
+const { spawnSync } = require('child_process');
+
+const args = process.argv;
+const nodeSh = args.shift();
+args.shift();
+const command = args.shift();
+
+switch(command){
+  case 'init':
+  case 'uninit':
+    spawnSync(nodeSh, ['./lib/' + command + '.js'].concat(args), {
+      cwd: __dirname,
+      stdio: 'inherit'
+    });
+    return;
+}
