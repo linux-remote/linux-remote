1972926263c9ac97b05e9fd3d56812cd3f75270d
tianxiazhizhong <tianxiazhizhong@gmail.com>
2020-01-01 16:19:36 +0800

    up  bak

--- index.js
+++ index.js
@@ -5,12 +5,28 @@ const nodeSh = args.shift();
 args.shift();
 const command = args.shift();
 
+
 switch(command){
   case 'init':
   case 'uninit':
-    spawnSync(nodeSh, ['./lib/' + command + '.js'].concat(args), {
-      cwd: __dirname,
-      stdio: 'inherit'
-    });
+    _spawn();
     return;
+}
+
+
+process.setuid('linux-remote');
+process.setgid('linux-remote');
+
+switch(command){
+  case 'init':
+  case 'uninit':
+    _spawn();
+    return;
+}
+
+function _spawn(){
+  spawnSync(nodeSh, ['./lib/' + command + '.js'].concat(args), {
+    cwd: __dirname,
+    stdio: 'inherit'
+  });
 }
