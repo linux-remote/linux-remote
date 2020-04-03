7571388a167244a25fa6d15e0050d73fc9ca0a4c
tianxiazhizhong <tianxiazhizhong@gmail.com>
2020-04-03 08:44:19 +0800

    version-map

--- index.js
+++ index.js
@@ -1,38 +1,51 @@
 #!/usr/bin/env node
 
-const { execSync } = require('child_process');
-const { username,  homeDir } = require('./lib/constant');
-const { warnLog } = require('./lib/util');
+"use strict";
 const os = require('os');
+const { username, homeDir } = require('./lib/constant');
+const { warnLog } = require('./lib/util');
+
 
 const args = process.argv;
-const nodeSh = args[0];
 const command = args[2];
-let params = args.slice(3);
-params = params.length ? ' ' + params.join(' ') : '';
-const cmd = `${nodeSh} ./lib/${command}.js${params}`;
 
 // 'root' user field:
-if(command === 'init' || 
-  command === 'uninit'){
-  execSync(cmd, {
-    cwd: __dirname,
-    stdio: 'inherit'
-  });
-  return;
+if(command === 'init'){
+
+  const init = require('./lib/init');
+  init();
+
+} else if(command === 'uninit'){
+
+  const uninit = require('./lib/uninit');
+  uninit();
+
+} else {
+  
+  // switch to 'linux-remote' user
+  const userInfo = os.userInfo();
+  if(userInfo.username !== username){
+
+    console.log(`You need run command as '${username}' user.`);
+    console.log(`You can use the following command to switch:`);
+    warnLog(`\nsudo su ${username} --shell='bin/bash'\n`);
+
+  } else {
+    const manageMPath = require.resolve('@linux-remote/manage', {
+      paths: [ homeDir + '/node_modules']
+    })
+    const managerHandler = require(manageMPath);
+    managerHandler(command);
+  }
+
+  // 'linux-remote' user field:
+  // proxy to @linux-remote/manage
+  // execSync(`linux-remote-manage ${command}${params}`, {
+  //   cwd: homeDir
+  // });
+
 }
 
-// switch to 'linux-remote' user
-const userInfo = os.userInfo();
-if(userInfo.username !== username){
-  console.log(`You need run command as '${username}' user.`);
-  console.log(`You can use the following command to switch:`);
-  warnLog(`\nsudo su ${username} --shell='bin/bash'\n`);
-  return;
-}
 
-// 'linux-remote' user field:
-// proxy to @linux-remote/manage
-execSync(`linux-remote-manage ${command}${params}`, {
-  cwd: homeDir
-});
+
+
