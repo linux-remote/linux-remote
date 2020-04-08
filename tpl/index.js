3aedbc40d02922df7280a46afdb0fba7b16c0fdd
tianxiazhizhong <tianxiazhizhong@gmail.com>
2020-04-08 15:47:31 +0800

    split 2 cli

--- tpl/index.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-process.env.LR_SERVER_PATH = require.resolve('@linux-remote/server');
-process.env.LR_USER_SERVER_PATH = require.resolve('@linux-remote/user-server');
-process.env.LR_LOGIN_BIN_PATH = path.join(__dirname, './bin/lr-login');
-
-require('@linux-remote/session-store');
--- up-version-map.js
+++ up-version-map.js
@@ -4,15 +4,43 @@ const fs = require('fs');
 const dir = process.cwd();
 const pkg = require(path.join(dir, 'package.json'));
 let name = pkg.name.split('/')[1];
-if(dir === __dirname){
-  name = 'cli';
-}
+
 const version = pkg.version;
 const mapPath = path.join(__dirname, 'docs/version-map.json');
 let map = fs.readFileSync(mapPath, 'utf-8');
 map = JSON.parse(map);
+
 if(map[name]){
+  map._total = _genTotalVersion(name, version);
   map[name] = version;
+  
   fs.writeFileSync(mapPath, JSON.stringify(map, null, ' '));
   console.log('linux-remote update versionMap success: ', name, version);
+  console.log(map._total);
+}
+
+function _genTotalVersion(name, newVersion){
+  let total = map._total; // 4.28.3.26-bata
+  total = total.split('-');
+  let totalSuffix = total[1];
+  if(totalSuffix){
+    totalSuffix = '-' + totalSuffix;
+  } else {
+    totalSuffix = '';
+  }
+  total = total[0];
+  total = total.split('.');
+  let mainVersion = total.shift();
+  
+  let currVersion = map[name];
+  currVersion = currVersion.split('.');
+  newVersion = newVersion.split('.');
+  total = total.map((num, i) => {
+    let totalN = Number(num) || 0;
+    let newN = Number(newVersion[i]) || 0;
+    let currN = Number(currVersion[i]) || 0;
+    let result = totalN + (newN - currN);
+    return result;
+  });
+  return `${mainVersion}.${total.join('.')}${totalSuffix}`;
 }
