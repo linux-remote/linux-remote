474315e4796eddca97b03c6fcb7f7a6b885369a4
tianxiazhizhong <tianxiazhizhong@gmail.com>
2020-04-10 17:23:13 +0800

    new index.html has version

---
+++ dev.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const PORT = 3005;
+const server = http.createServer(function(req, res){
+  console.log(req.method, req.url);
+  let file = req.url.substr(1);
+  if(!file){
+    file = 'index.html';
+  }
+  fs.readFile(path.join(__dirname, 'docs/' + file), function(err, content){
+    if(err){
+      res.statusCode = 500;
+      res.end(err.name + ':' + err.message);
+    } else {
+      let i = file.lastIndexOf('.');
+      if(i !== -1){
+        let suffix = file.substr(i + 1);
+        if(suffix === 'svg'){
+          res.setHeader('content-type', 'image/svg+xml');
+        }
+      }
+      res.end(content);
+    }
+  })
+})
+
+server.listen(PORT);
+
+server.on('listening', function(){
+  console.log('server listening', PORT);
+})
