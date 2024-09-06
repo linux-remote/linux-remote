793b671cb92114143314fc65e71f21399260fbe1
hezedu <hezedu@hotmail.com>
2024-09-06 09:51:56 +0800

    make strongs https://github.com/linux-remote/linux-remote/pull/246

--- dev.js
+++ dev.js
@@ -2,34 +2,34 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const PORT = 3005;
+const files = fs.readdirSync(path.join(__dirname, 'docs'));
 const server = http.createServer(function(req, res){
   console.log(req.method, req.url);
   let file = req.url.substr(1);
   if(!file){
     file = 'index.html';
   }
-  const files = fs.readdirSync(path.join(__dirname, 'docs'));
-  if(!files.includes(file)){
+  if(files.includes(file)){
+    fs.readFile(path.join(__dirname, 'docs/' + file), function(err, content){
+      if(err){
+        res.statusCode = 500;
+        res.end(err.name + ':' + err.message);
+      } else {
+        let i = file.lastIndexOf('.');
+        if(i !== -1){
+          let suffix = file.substr(i + 1);
+          if(suffix === 'svg'){
+            res.setHeader('content-type', 'image/svg+xml');
+          }
+        }
+        res.end(content);
+      }
+    });
+  } else {
     res.statusCode = 404;
     res.end('notFound');
     return;
   }
-
-  fs.readFile(path.join(__dirname, 'docs/' + file), function(err, content){
-    if(err){
-      res.statusCode = 500;
-      res.end(err.name + ':' + err.message);
-    } else {
-      let i = file.lastIndexOf('.');
-      if(i !== -1){
-        let suffix = file.substr(i + 1);
-        if(suffix === 'svg'){
-          res.setHeader('content-type', 'image/svg+xml');
-        }
-      }
-      res.end(content);
-    }
-  })
 })
 
 server.listen(PORT);
